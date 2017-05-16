b9e55d48020c7fdf8eab248635b95e28e4641344
Beto Montejo <beto.montejo@metapack.com>
2017-05-16 14:31:58 +0100

    Fix typo on bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install -y python-minimal build-essential libssl-dev python-dev
-get -O - https://bootstrap.pypa.io/get-pip.py | sudo python
+wget -O - https://bootstrap.pypa.io/get-pip.py | sudo python
 sudo pip install ansible
 sudo mkdir /etc/ansible && echo "localhost ansible_connection=local" | sudo tee --append /etc/ansible/hosts
 ansible-playbook system-bootstrap.yml
