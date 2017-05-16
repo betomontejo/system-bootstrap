3b6eb88debfeddaae221fbb8a1de4033ccab2f4a
Beto Montejo <beto.montejo@metapack.com>
2017-05-16 14:21:54 +0100

    First commit, initial bootstrap script

---
+++ Vagrantfile
@@ -0,0 +1,14 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box"
+  config.vm.host_name = "blade"
+
+  config.vm.provider "virtualbox" do |vb|
+     vb.memory = "1024"
+     vb.cpus = 1
+   end
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y python-minimal build-essential libssl-dev python-dev
+get -O - https://bootstrap.pypa.io/get-pip.py | sudo python
+sudo pip install ansible
+sudo mkdir /etc/ansible && echo "localhost ansible_connection=local" | sudo tee --append /etc/ansible/hosts
+ansible-playbook beto-system.yml
+
